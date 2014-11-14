7f964b6652bc426ecab794b4342aa0cf7fc4dff0
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-11-14 16:57:49 -0500

    nn

--- config/initializers/yummly.rb
+++ config/initializers/yummly.rb
@@ -1,6 +1,6 @@
 
 Yummly.configure do |config|
-  config.app_id = ENV['YUM_ID']
-  config.app_key = ENV['YUM_KEY']
+  config.app_id = '62ddf118'#ENV['YUM_ID']
+  config.app_key = '453d674acefb757b123376c4557bf17d'#ENV['YUM_KEY']
   config.use_ssl = false # Default is false
 end
