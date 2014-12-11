0df9f2bb2386f2a3b796c4aec8842bd4c1d4e703
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-12-10 21:59:15 -0500

    Nixon merged his testing branch with master locally and is about to push to heroku

--- config/initializers/yummly.rb
+++ config/initializers/yummly.rb
@@ -1,6 +1,6 @@
 
 Yummly.configure do |config|
-  config.app_id = '5a7c6fe5' #ENV['YUM_ID']
-  config.app_key = '53438920b99c5d6b3f37fccf87dfb262' #ENV['YUM_KEY']
+  config.app_id = ENV['YUM_ID']
+  config.app_key = ENV['YUM_KEY']
   config.use_ssl = false # Default is false
 end
