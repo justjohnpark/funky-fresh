e5243c631e0f7b11f130d6db8efc4c1e244f0cc2
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-12-29 01:57:07 -0500

    recipe logic && view refactored

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,11 @@
 class RecipesController < ApplicationController
 	include RecipeHelper
 	def index
+		yummly_worker = YummlyWorker.new
 		@user = User.find(params[:user_id])
 		@pantry = Pantry.find(params[:pantry_id])
 		@items = @pantry.recipe_search_parameters
-		@search_results = search_yummly_for(@items)
-		@best_matches = best_matches(@search_results ,@pantry) unless @search_results.matches.empty?
+		@search_results = yummly_worker.search_yummly_for(@items)
+		@best_matches = yummly_worker.best_matches(@search_results ,@pantry) unless @search_results.matches.empty?
 	end
-end
-
-class Hash
-  def only(*whitelist)
-    {}.tap do |h|
-      (keys & whitelist).each { |k| h[k] = self[k] }
-    end
-  end
 end
--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -1,24 +1,36 @@
 module RecipeHelper
-	def search_yummly_for(ingredients) 
-		Yummly.search(ingredients, 'maxResult' => 5)
-	end
 
-	def sort_matches(matches)
-		matches.sort{|a, b| [b['pantry_has'].length, b['rating']]<=> [a['pantry_has'].length, a['rating']]}
+	class Hash
+	  def only(*whitelist)
+	    {}.tap do |h|
+	      (keys & whitelist).each { |k| h[k] = self[k] }
+	    end
+	  end
 	end
+	
+	class YummlyWorker
 
-	def get_relevant_data(matches)
-		matches.each do |match, values|
-			recipe = Yummly.find(match['id'])
-			match['recipe'] = recipe.json.only 'attribution', 'name', 'rating'
-			match['recipe']['ingredients'] = recipe.ingredients
-			match['recipe']['thumbnail'] = recipe.thumbnail
+		def search_yummly_for(ingredients) 
+			Yummly.search(ingredients, 'maxResult' => 5)
 		end
-	end
 
-	def best_matches(search_results, pantry)
-		matches = search_results.matches.map{ |match| {'id' => match['id'], 'pantry_has' => pantry.pantry_has(match['ingredients']).join(', ') , 'pantry_might_have' => pantry.pantry_might_have(match['ingredients']).join(', '), 'pantry_missing' => pantry.pantry_missing(match['ingredients']).join(', '), 'sourceDisplayName' => match['sourceDisplayName']} }
-		recipes = get_relevant_data(matches)
-		sorted_matches = sort_matches(recipes)
+		def sort_matches(matches)
+			matches.sort{|a, b| [b['pantry_has'].length, b['rating']]<=> [a['pantry_has'].length, a['rating']]}
+		end
+
+		def get_relevant_data(matches)
+			matches.each do |match, values|
+				recipe = Yummly.find(match['id'])
+				match['recipe'] = recipe.json.only 'attribution', 'name', 'rating'
+				match['recipe']['ingredients'] = recipe.ingredients
+				match['recipe']['thumbnail'] = recipe.thumbnail
+			end
+		end
+
+		def best_matches(search_results, pantry)
+			matches = search_results.matches.map{ |match| {'id' => match['id'], 'pantry_has' => pantry.pantry_has(match['ingredients']).join(', ') , 'pantry_might_have' => pantry.pantry_might_have(match['ingredients']).join(', '), 'pantry_missing' => pantry.pantry_missing(match['ingredients']).join(', '), 'sourceDisplayName' => match['sourceDisplayName']} }
+			recipes = get_relevant_data(matches)
+			sorted_matches = sort_matches(recipes)
+		end
 	end
 end
